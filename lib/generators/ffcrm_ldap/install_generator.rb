5a2a024720906bc354b1e0eda6fe85f17269f0e1
bwxx <bwx@outlook.de>
2016-05-01 12:08:20 +0100

    Revert "renamed generator to install"
    This reverts commit 1e93febc00aad5f9d3581487e03246462782a27f.

--- lib/generators/ffcrm_ldap/install_generator.rb
+++
@@ -1,9 +0,0 @@
-module FfcrmLdap
-  class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('../templates', __FILE__)
-    def copy_config
-      copy_file "config/ldap.yml.example"
-      copy_file "config/ldap_attributes_map.yml.example"
-    end
-  end
-end
